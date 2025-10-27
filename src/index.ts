979c3f9556800382ae67f87d34d045423b6ec0f0
PoonamMehan <poonammehan655@gmail.com>
2025-10-27 12:03:20 +0530

    helper for e2b file create added

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { google } from "@ai-sdk/google";
 import { SYSTEM_PROMPT } from "./prompt.ts";
 import { updateFile, readFile } from "../tools/index.ts";
 import cors from "cors";
+import { CreateTreeFile } from "../helper/create-tree-file.ts";
 dotenv.config();
 
 const app = express();
@@ -49,13 +50,7 @@ app.post("/chat", async (req, res) => {
       updateFile: updateFile(sbx),
     },
     maxRetries: 0,
-    messages: [
-      ...messages,
-      {
-        role: "user",
-        content: prompt,
-      },
-    ],
+    messages,
   });
 
   const host = sbx.getHost(5173);
@@ -68,10 +63,6 @@ app.post("/chat", async (req, res) => {
   res.json({
     messages: [
       ...messages,
-      {
-        role: "user",
-        content: prompt,
-      },
       {
         role: "assistant",
         content: JSON.stringify(resp, null, 2),
@@ -98,87 +89,7 @@ app.post("/files", async (req, res) => {
     timeoutMs: 9_00_000,
   });
 
-  await sbx.files.write(
-    "/tmp/getTree.js",
-    `
-    const fs = require('fs');
-    const path = require('path');
-
-    const getFileAndTree = (projectDir) => {
-      // console.log("Project Dir is : ", projectDir);
-      // console.log("is this exists : " , fs.existsSync(projectDir))
-
-      if (!fs.existsSync(projectDir)) {
-        console.error(\`Base directory not found: \${projectDir}\`);
-      }
-
-      const files = [];
-
-      const walk = (dir) => {
-        const items = fs.readdirSync(dir);
-        for (const item of items) {
-          if (
-            ['node_modules', '.git', 'dist', 'build', '.bash_logout', '.bashrc', 'package-lock.json', ".npm", ".cache", "core"].includes(item)
-          ) {
-          continue
-          };
-
-          const fullPath = path.join(dir, item);
-          // console.log("searching path : ", fullPath)
-          if (fs.statSync(fullPath).isDirectory()) {
-            walk(fullPath);
-          } else {
-            const content = fs.readFileSync(fullPath, 'utf8');
-            // console.log("pushing to files : ", path.relative(projectDir, fullPath))
-            files.push({
-              path: path.relative(projectDir, fullPath),
-              code: content,
-            });
-          }
-        }
-
-      };
-
-      walk(projectDir);
-
-
-
-      const tree = {};
-      files.forEach(({ path: filePath }) => {
-        const parts = filePath.split(path.sep);
-        parts.forEach((part, index) => {
-          const key = parts.slice(0, index + 1).join('/');
-          if (!tree[key]) {
-            tree[key] = { name: part };
-          }
-          if (index < parts.length - 1) {
-            tree[key].children ||= [];
-            const childKey = parts.slice(0, index + 2).join('/');
-            if (!tree[key].children.includes(childKey)) {
-              tree[key].children.push(childKey);
-            }
-          }
-        });
-      });
-
-      // console.log("file are : ",files )
-      // console.log("tree is : ",tree )
-
-      return {
-      files,
-      tree
-      }
-    };
-
-    // console.log("current path is : " , path.resolve("."))
-
-
-    const projectDir = path.resolve(".")
-    const result = getFileAndTree(projectDir)
-
-    console.log(JSON.stringify(result));
-    `,
-  );
+  await CreateTreeFile(sbx);
 
   const execution = await sbx.commands.run("node /tmp/getTree.js");
   // console.log("execution.stdout : ", execution.stdout);
